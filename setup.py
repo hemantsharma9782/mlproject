21ce5be0fff801861df7d441fbc8e6b17d0ecbf8
hemantsharma9782 <hemantsharma2004hs@gmail.com>
2026-03-11 23:56:26 +0530

    Loggind and Exception

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT='-e .'
+
+def get_requirement(file_path:str)->List[str]:
+    ''' this return list of requirement'''
+    requirements=[]
+    with open (file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements        
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Hemant Sharma',
+    author_email='hemantsharma2004hs@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirement('requirement.txt')
+)
